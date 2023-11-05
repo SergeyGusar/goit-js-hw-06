e57dacfaa74c242b0a4b5878ff59746c1d6be2dc
Sergey <110050958+SergeyGusar@users.noreply.github.com>
2023-11-05 20:07:13 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.getElementById("ingredients");
+
+const listItems = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  return listItem;
+});
+
+ingredientsList.append(...listItems);
