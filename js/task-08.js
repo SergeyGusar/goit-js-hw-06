b75fb58b37ac1d05ce8b134a5c20d56cacbc5d3f
Sergey <110050958+SergeyGusar@users.noreply.github.com>
2023-11-06 18:22:47 +0200

    task 10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault(); // Предотвращаем перезагрузку страницы
+
+  const formData = new FormData(loginForm);
+  const formObject = {};
+
+  formData.forEach((value, key) => {
+    formObject[key] = value;
+  });
+
+  if (!formObject.email || !formObject.password) {
+    alert('Please fill in all fields.');
+  } else {
+    console.log(formObject);
+    loginForm.reset(); // Очищаем значения полей формы
+  }
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+const input = document.querySelector('input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  const initialSize = 30;
+  const fragment = document.createDocumentFragment();
+  
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    const size = initialSize + i * 10;
+    const color = getRandomHexColor();
+    
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    div.style.backgroundColor = color;
+    
+    fragment.appendChild(div);
+  }
+  
+  boxesDiv.innerHTML = '';
+  boxesDiv.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
